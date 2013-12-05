dfe804e59a44cf13697cff9cc975fabfb7359d85
Takashi Sasaki <t.sasaki@bmandw.com>
2013-12-05 15:14:52 +0900

    VCランタイムを同梱するように修正した

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from glob import glob
+import os
 import py2exe
+import shutil
+
+candidates = glob(r"C:\Windows\winsxs\Manifests\x86_microsoft.vc90.crt_*_9.0.21022.8_*.manifest")
+if not candidates:
+    raise Exception("Coud not find VC9 runtime ( 9.0.21022.8 ) from 'C:\Windows\winsxs'.")
+
+dirname = os.path.splitext(os.path.basename(candidates[0]))[0]
+data_files = [("Microsoft.VC90.CRT", [candidates[0]] + glob(os.path.join(r"C:\Windows\winsxs", dirname, "*.*")))]
+
+build_dir = "dist"
 
 option = {
     "compressed"    :    1    ,
@@ -18,5 +30,9 @@ setup(
         {"script"   :    "hwaconv.py"}
     ],
 
+    data_files=data_files,
     zipfile = None
 )
+
+manifests = glob(os.path.join(build_dir, r"Microsoft.VC90.CRT", "*.manifest"))
+shutil.move(manifests[0], os.path.join(build_dir, "Microsoft.VC90.CRT", "Microsoft.VC90.CRT.manifest"))
